b398464a087972ff5425e64784d3d82db2250c27
KatanaNevermore <grenkachannel@hotmail.com>
2023-10-20 18:56:08 +0300

    Работа со статусами перенесена на формы

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -3,7 +3,9 @@ from django.views import View
 from task_manager.statuses.models import Status
 from django.contrib import messages
 import rollbar
-
+from task_manager.statuses.forms import StatusForm
+from task_manager.services import handle_error, handle_success
+from task_manager.statuses import services
 
 class IndexView(View):
 
@@ -24,47 +26,36 @@ class CreateStatusView(View):
 
     def get(self, request, *args, **kwargs):
         is_session_active = 'user_id' in request.session
+        form = StatusForm()
         if is_session_active:
             rollbar.report_exc_info()
-            return render(request, 'statuses/create.html', {'is_session_active': is_session_active})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
-
+            return render(request, 'statuses/create.html', {'is_session_active': is_session_active, 'form': form})
+        return handle_error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
     def post(self, request, *args, **kwargs):
-        name = request.POST.get('name')
-        status = Status()
-        status.name = name
-        status.save()
-        rollbar.report_exc_info()
-        messages.success(request, 'Статус успешно создан')
-        return redirect('statuses_index')
+        form = StatusForm(request.POST)
+        if services.create_status(form):
+            return handle_success(request, 'Статус успешно создан', 'statuses_index')
 
 
 class UpdateStatusView(View):
 
     def get(self, request, *args, **kwargs):
+        status_id = kwargs.get('pk')
         is_session_active = 'user_id' in request.session
         if is_session_active:
-            status_id = kwargs.get('pk')
-            status = Status.objects.get(id=status_id)
+            initial_data = services.get_initial_data(status_id)
+            form = StatusForm(initial_data)
             rollbar.report_exc_info()
             return render(request, 'statuses/update.html',
                           {'is_session_active': is_session_active,
-                           'status': status})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
+                           'form': form})
+        return handle_error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
     def post(self, request, *args, **kwargs):
         status_id = kwargs.get('pk')
-        name = request.POST.get('name')
-        status = Status.objects.get(id=status_id)
-        status.name = name
-        status.save()
-        rollbar.report_exc_info()
-        messages.success(request, 'Статус успешно изменен')
-        return redirect('statuses_index')
+        form = StatusForm(request.POST)
+        if services.update_status(status_id, form):
+            return handle_success(request, 'Статус успешно изменен','statuses_index')
 
 
 class DeleteStatusView(View):
@@ -78,14 +69,10 @@ class DeleteStatusView(View):
             return render(request, 'statuses/delete.html',
                           {'is_session_active': is_session_active,
                            'status': status})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
+        return handle_error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
     def post(self, request, *args, **kwargs):
+
         status_id = kwargs.get('pk')
-        status = Status.objects.get(id=status_id)
-        status.delete()
-        rollbar.report_exc_info()
-        messages.error(request, 'Статус успешно удален')
-        return redirect('statuses_index')
+        if services.delete_status(status_id):
+            handle_success(request, 'Статус успешно удален', 'statuses_index')
