cc709b9a4d74b7fc928ab1add2b20ffb7211b66a
KatanaNevermore <grenkachannel@hotmail.com>
2023-10-22 14:48:45 +0300

    FIx

--- task_manager/labels/forms.py
+++ task_manager/labels/forms.py
@@ -1,15 +1,13 @@
 from django import forms
-from task_manager.labels.models import Labels
+
 
 class LabelForm(forms.Form):
-    class Meta:
-        model = Labels
-        fields = ['name']
-        label = 'Имя',
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя',
-                'required': 'required',
-            }),
-        }
+    name = forms.CharField(
+        label='Имя',
+        label_suffix='',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Имя',
+            'required': 'required',
+        }))
--- task_manager/statuses/forms.py
+++ task_manager/statuses/forms.py
@@ -1,15 +1,13 @@
 from django import forms
-from task_manager.statuses.models import Status
+
 
 class StatusForm(forms.Form):
-    class Meta:
-        model = Status
-        fields = ['name']
+    name = forms.CharField(
         label='Имя',
-        widgets = {
-            'name': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя',
-                'required': 'required',
-            }),
-        }
+        label_suffix='',
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Имя',
+            'required': 'required',
+        }))
