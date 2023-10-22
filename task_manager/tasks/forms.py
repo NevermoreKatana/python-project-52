5e01b6ae2d8a891ea687e1d57b9541096d900067
KatanaNevermore <grenkachannel@hotmail.com>
2023-10-22 13:50:55 +0300

    Задачи перенесены на формы

---
+++ task_manager/tasks/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from task_manager.statuses.models import Status
+from task_manager.labels.models import Labels
+from django.contrib.auth.models import User
+
+class TaskForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=150, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя'}))
+    description = forms.CharField(label='Описание', widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Описание'}))
+    status = forms.ChoiceField(label='Статус', choices=[('', '---------')] + [(status.id, status.name) for status in Status.objects.all()], widget=forms.Select(attrs={'class': 'form-select'}))
+    executor = forms.ChoiceField(label='Исполнитель', choices=[('', '---------')] + [(executor.id, f"{executor.first_name} {executor.last_name}") for executor in User.objects.all()], widget=forms.Select(attrs={'class': 'form-select'}))
+    labels = forms.MultipleChoiceField(label='Метки', choices=[(label.id, label.name) for label in Labels.objects.all()], widget=forms.SelectMultiple(attrs={'class': 'form-select', 'multiple': 'multiple'}))
+
+    def __init__(self, *args, **kwargs):
+        name = kwargs.pop('name', None)
+        description = kwargs.pop('description', None)
+        status_id = kwargs.pop('status_id', None)
+        executor_id = kwargs.pop('executor_id', None)
+        labels = kwargs.pop('labels', None)
+
+        super(TaskForm, self).__init__(*args, **kwargs)
+
+
+        if name:
+            self.initial['name'] = name
+
+        if description:
+            self.initial['description'] = description
+
+        if status_id:
+            self.initial['status'] = status_id
+
+        if executor_id:
+            self.initial['executor'] = executor_id
+
+        if labels:
+            self.initial['labels'] = labels
+
+
+class TaskFilterForm(forms.Form):
+    status = forms.ChoiceField(
+        label='Статус',
+        choices=[('', '---------')] + [(status.id, status.name) for status in Status.objects.all()],
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select '})
+    )
+    executor = forms.ChoiceField(
+        label='Исполнитель',
+        choices=[('', '---------')] + [(u.id, f"{u.first_name} {u.last_name}") for u in User.objects.all()],
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select '})
+    )
+    label = forms.ChoiceField(
+        label='Метка',
+        choices=[('', '---------')] + [(label.id, label.name) for label in Labels.objects.all()],
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-select '})
+    )
+    self_tasks = forms.BooleanField(
+        label='Только свои задачи',
+        required=False,
+        widget=forms.CheckboxInput(attrs={'class': 'form-check-input'})
+    )
+
+
+
--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -6,87 +6,41 @@ from task_manager.statuses.models import Status
 from task_manager.tasks.models import Tasks
 from task_manager.labels.models import Labels
 import rollbar
-
+from task_manager.services import handle_success, handle_error
+from task_manager.tasks import services
+from task_manager.tasks.forms import TaskForm, TaskFilterForm
 
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
         is_session_active = 'user_id' in request.session
         if is_session_active:
-            tasks = Tasks.objects.all()
-            statuses = Status.objects.all()
-            users = User.objects.all()
-            labels = Labels.objects.all()
-
-            status_id = request.GET.get('status')
-            executor_id = request.GET.get('executor')
-            label_id = request.GET.get('label')
-            self_tasks = request.GET.get('self_tasks')
-
-            if status_id:
-                tasks = tasks.filter(status__id=status_id)
-            if executor_id:
-                tasks = tasks.filter(executor__id=executor_id)
-            if label_id:
-                tasks = tasks.filter(labels__id=label_id)
-
-            if self_tasks:
-                tasks = tasks.filter(author=request.user)
-
+            form = TaskFilterForm(request.GET)
+            tasks = services.task_filter(form, request)
             return render(request, 'tasks/index.html', {
                 'is_session_active': is_session_active,
                 'tasks': tasks,
-                'statuses': statuses,
-                'users': users,
-                'labels': labels,
-                'status_id': int(status_id) if status_id else status_id,
-                'executor_id': int(executor_id) if executor_id else executor_id,
-                'label_id': int(label_id) if label_id else label_id,
-                'self_tasks': self_tasks,
+                'form': form,
             })
 
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        return redirect('login')
-
+        return handle_error(request,'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
 class TasksCreateView(View):
 
     def get(self, request, *args, **kwargs):
         is_session_active = 'user_id' in request.session
         if is_session_active:
-            statuses = list(Status.objects.all())
-            user = User.objects.values('id', 'first_name', 'last_name')
-            user = list(user)
-            labels = Labels.objects.all()
+            form = TaskForm()
             rollbar.report_exc_info()
             return render(request, 'tasks/create.html',
                           {'is_session_active': is_session_active,
-                           'statuses': statuses,
-                           'user': user,
-                           'labels': labels})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
+                           'form': form})
+        return handle_error(request,'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
     def post(self, request, *args, **kwargs):
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        status = request.POST.get('status')
-        executor = request.POST.get('executor')
-        labels = request.POST.getlist('labels')
-        task = Tasks()
-        task.name = name
-        task.description = description
-        task.status = Status.objects.get(id=status)
-        task.executor = User.objects.get(id=executor)
-        task.author = User.objects.get(id=request.session.get('user_id'))
-        task.save()
-        for label_id in labels:
-            label = Labels.objects.get(id=label_id)
-            task.labels.add(label)
-        rollbar.report_exc_info()
-        messages.success(request, 'Задача успешно создана')
-        return redirect('tasks_index')
+        form = TaskForm(request.POST)
+        if services.create_task(form, request):
+            return handle_success(request, 'Задача успешно создана','tasks_index')
 
 
 class TasksDeleteView(View):
@@ -95,74 +49,39 @@ class TasksDeleteView(View):
         task_id = kwargs.get('pk')
         is_session_active = 'user_id' in request.session
         if is_session_active:
-            task = Tasks.objects.values('name', 'author').filter(id=task_id)
-            if request.session.get('user_id') != task[0]['author']:
-                messages.error(request, 'Задачу может удалить только ее автор')
+            task = services.not_author_delete(request, task_id)
+            if task:
                 rollbar.report_exc_info()
-                return redirect('tasks_index')
-            task = list(task)
-            rollbar.report_exc_info()
-            return render(request, 'tasks/delete.html',
-                          {'is_session_active': is_session_active,
-                           'task': task[0]})
-        rollbar.report_exc_info()
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        return redirect('login')
+                return render(request, 'tasks/delete.html',
+                              {'is_session_active': is_session_active,
+                               'task': task})
+            return handle_error(request, 'Задачу может удалить только ее автор', 'tasks_index')
+        return handle_error(request,'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
     def post(self, request, *args, **kwargs):
         task_id = kwargs.get('pk')
-        task = Tasks.objects.get(id=task_id)
-        if task:
-            task.delete()
-            messages.error(request, 'Задача успешно удалена')
-            rollbar.report_exc_info()
-            return redirect('tasks_index')
+        if services.delete_task(task_id):
+            return handle_success(request, 'Задача успешно удалена', 'tasks_index')
 
 
 class UpdateStatusView(View):
 
     def get(self, request, *args, **kwargs):
         is_session_active = 'user_id' in request.session
+        task_id = kwargs.get('pk')
         if is_session_active:
-            task_id = kwargs.get('pk')
-            statuses = list(Status.objects.all())
-            user = User.objects.values('id', 'first_name', 'last_name')
-            user = list(user)
-            labels = Labels.objects.all()
-            task = Tasks.objects.get(id=task_id)
+            form = services.get_init_update_task(task_id)
             rollbar.report_exc_info()
             return render(request, 'tasks/update.html',
                           {'is_session_active': is_session_active,
-                           'statuses': statuses,
-                           'user': user,
-                           'task': task,
-                           'labels': labels})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
+                           'form': form})
+        return handle_error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
 
     def post(self, request, *args, **kwargs):
-        status_id = kwargs.get('pk')
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        status = request.POST.get('status')
-        executor = request.POST.get('executor')
-        labels = request.POST.getlist('labels')
-        task = Tasks.objects.get(id=status_id)
-        task.name = name
-        task.description = description
-        task.status = Status.objects.get(id=status)
-        task.executor = User.objects.get(id=executor)
-        task.author = User.objects.get(id=request.session.get('user_id'))
-        task.labels.clear()
-        task.save()
-        for label_id in labels:
-            label = Labels.objects.get(id=label_id)
-            task.labels.add(label)
-
-        messages.success(request, 'Задача успешно изменена')
-        rollbar.report_exc_info()
-        return redirect('tasks_index')
+        task_id = kwargs.get('pk')
+        form = TaskForm(request.POST)
+        if services.update_task(task_id, form, request):
+            return handle_success(request, 'Задача успешно изменена', 'tasks_index')
 
 
 class TaskView(View):
@@ -176,6 +95,4 @@ class TaskView(View):
             return render(request, 'tasks/task.html',
                           {'is_session_active': is_session_active,
                            'tasks': tasks})
-        messages.error(request, 'Вы не авторизованы! Пожалуйста, выполните вход.')
-        rollbar.report_exc_info()
-        return redirect('login')
+        return handle_error(request,'Вы не авторизованы! Пожалуйста, выполните вход.', 'login')
