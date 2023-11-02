95b635f511bcb01668441ca6cdcc0b4e81a9e272
KatanaNevermore <grenkachannel@hotmail.com>
2023-11-02 10:14:21 +0300

    update

--- task_manager/services.py
+++ task_manager/services.py
@@ -1,28 +1,25 @@
 import rollbar
 from django.contrib import messages
-from django.shortcuts import redirect
+
 from django.contrib.auth import authenticate, login
 
 
-def handle_error(request, message, redirect_url, report_exception=True):
+def handle_error(request, message, report_exception=True):
     if report_exception:
         rollbar.report_exc_info()
     messages.error(request, message)
-    return redirect(redirect_url)
 
 
-def handle_success(request, message, redirect_url, report_exception=True):
+def handle_success(request, message,report_exception=True):
     if report_exception:
         rollbar.report_exc_info()
     messages.success(request, message)
-    return redirect(redirect_url)
 
 
-def handle_info(request, message, redirect_url, report_exception=True):
+def handle_info(request, message, report_exception=True):
     if report_exception:
         rollbar.report_exc_info()
     messages.info(request, message)
-    return redirect(redirect_url)
 
 
 def login_user(form, request):
--- task_manager/views.py
+++ task_manager/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 import rollbar
 from task_manager.forms import LoginForm
 from task_manager import services
+from django.shortcuts import redirect
 
 
 class IndexView(View):
@@ -26,7 +27,8 @@ class LoginView(View):
     def post(self, request, *args, **kwargs):
         form = LoginForm(request.POST)
         if services.login_user(form, request):
-            return services.handle_success(request, 'Вы залогинены', 'main')
+            services.handle_success(request, 'Вы залогинены')
+            return redirect('main')
         initial_data = services.initial_login_data(form)
         form = LoginForm(initial_data, is_valid=True)
 
@@ -40,4 +42,5 @@ class LogoutView(View):
 
     def post(self, request, *args, **kwargs):
         logout(request)
-        return services.handle_info(request, 'Вы разлогинены', 'main')
+        services.handle_info(request, 'Вы разлогинены')
+        return redirect('main')
